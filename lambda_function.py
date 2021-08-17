2feb4c7675fdd9fe92b605cd471f1f224d7bbdd2
Deains <deinsid@gmail.com>
2021-08-18 00:28:17 +0300

    Test

--- lambda_function.py
+++ lambda_function.py
@@ -1,22 +1,22 @@
 import json
-import os
-import time
+# import os
+# import time
+#
+# import requests
 
-import requests
 
-
-class CircularBuffer:
-    def __init__(self, array):
-        self.max_size = len(array)
-        self.pointer = 0
-        self.buffer = array
-
-    def get_three_items(self):
-        while True:
-            yield [self.buffer[self.pointer], self.buffer[(self.pointer + 1) % self.max_size],
-                   self.buffer[(self.pointer + 2) % self.max_size]]
-            time.sleep(3)
-            self.pointer = (self.pointer + 1) % self.max_size
+# class CircularBuffer:
+#     def __init__(self, array):
+#         self.max_size = len(array)
+#         self.pointer = 0
+#         self.buffer = array
+#
+#     def get_three_items(self):
+#         while True:
+#             yield [self.buffer[self.pointer], self.buffer[(self.pointer + 1) % self.max_size],
+#                    self.buffer[(self.pointer + 2) % self.max_size]]
+#             time.sleep(3)
+#             self.pointer = (self.pointer + 1) % self.max_size
 
 
 def lambda_handler(event, context):
