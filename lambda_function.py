567c55ca900d059530d161ffcaa5ce5d2bd261be
Deains <deinsid@gmail.com>
2021-08-18 00:17:08 +0300

    Test

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,39 @@
 import json
+import os
+import time
+
+import requests
+
+
+class CircularBuffer:
+    def __init__(self, array):
+        self.max_size = len(array)
+        self.pointer = 0
+        self.buffer = array
+
+    def get_three_items(self):
+        while True:
+            yield [self.buffer[self.pointer], self.buffer[(self.pointer + 1) % self.max_size],
+                   self.buffer[(self.pointer + 2) % self.max_size]]
+            time.sleep(3)
+            self.pointer = (self.pointer + 1) % self.max_size
 
 
 def lambda_handler(event, context):
+    url = "https://api.airtable.com/v0/appDgJgMhHkLteXj9/MainTable?view=Grid%20view"
+    key = "Bearer " + os.environ['API_KEY']
+    headers = {
+        "Authorization": key
+    }
+    response = requests.get(url, headers=headers)
+    records = json.loads(response.text)['records']
+
+    titles = []
+    for record in records:
+        titles.append(record['fields']['title'])
+
+    cb = CircularBuffer(titles)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello World')
+        'body': json.dumps(key)
     }
