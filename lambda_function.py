13dec80756ada40d20ce7e04c91fb9c693280bbc
Deains <deinsid@gmail.com>
2021-08-18 23:39:42 +0300

    Test

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 import json
 import os
-import time
+import threading
 
 import requests
 
@@ -12,16 +12,15 @@ class CircularBuffer:
         self.buffer = array
 
     def get_three_items(self):
-        while True:
-            yield [self.buffer[self.pointer], self.buffer[(self.pointer + 1) % self.max_size],
-                   self.buffer[(self.pointer + 2) % self.max_size]]
-            time.sleep(3)
-            self.pointer = (self.pointer + 1) % self.max_size
+        three_items = [self.buffer[self.pointer], self.buffer[(self.pointer + 1) % self.max_size],
+                       self.buffer[(self.pointer + 2) % self.max_size]]
+        self.pointer = (self.pointer + 1) % self.max_size
+        return three_items
 
 
-def lambda_handler(event, context):
+def main():
     url = "https://api.airtable.com/v0/appDgJgMhHkLteXj9/MainTable?view=Grid%20view"
-    key = "Bearer " + os.environ['API_KEY']
+    key = "Bearer " + os.environ["API_KEY"]
     headers = {
         "Authorization": key
     }
@@ -31,9 +30,26 @@ def lambda_handler(event, context):
     titles = []
     for record in records:
         titles.append(record['fields']['title'])
-
-    cb = CircularBuffer(titles)
+    cb.buffer = titles
+    cb.max_size = len(titles)
     return {
         'statusCode': 200,
-        'body': json.dumps(titles, ensure_ascii=False).encode('utf8')
+        'body': json.dumps(" ".join(cb.get_three_items()), ensure_ascii=False).encode('utf8')
     }
+
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+cb = CircularBuffer([])
+
+
+def lambda_handler(event, context):
+    set_interval(main, 1)
